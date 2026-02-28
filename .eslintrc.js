9c7e34aba095f21ad051d7bf440550c97e51452d
gopiprsanthp <potipireddygopiprasanth@gmail.com>
2026-02-28 20:32:34 +0530

    fix: update Prettier configuration and add linting scripts to package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,14 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
-  }
+  },
+  overrides: [
+    {
+      files: ['tests/**/*.{ts,tsx,js,jsx}'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
+    }
+  ]
 };
